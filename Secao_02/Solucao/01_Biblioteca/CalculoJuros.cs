658d0605732b5710d4e8766cd7e3d79ec0257c51
Bruno Ferreira <bruno.olympio.ferreira@gmail.com>
2022-11-26 18:40:36 -0300

    Secao 02 => Referencias

---
+++ Secao_02/Solucao/01_Biblioteca/CalculoJuros.cs
@@ -0,0 +1,10 @@
+﻿namespace _01_Biblioteca
+{
+    public class CalculoJuros
+    {
+        public double Soma(int A, int B)
+        {
+            return A + B;
+        }
+    }
+}
---
+++ Secao_02/Solucao/01_ReferenciaPrograma/Program.cs
@@ -0,0 +1,7 @@
+﻿using _01_Biblioteca;
+
+CalculoJuros calc = new CalculoJuros();
+double resultado = calc.Soma(10, 20);
+
+Console.WriteLine("Resultado da soma: " + resultado);
+Console.ReadKey();
